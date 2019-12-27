f577ac5276ad5a41d1c1885b7295a251509041e5
mario.alvarez@consensys.net <yorix@DESKTOP-84OVJN9.localdomain>
2019-12-27 13:15:05 -0800

    changing tuple encoding for easier termination proof

--- AbiTypes.thy
+++ AbiTypes.thy
@@ -351,6 +351,12 @@ fun tails_measure :: "(abi_value + (abi_type * nat)) list \<Rightarrow> nat" whe
 | "tails_measure ((Inr (t, _))#ts) =
     abi_type_measure t + tails_measure ts"
 
+(* TODO: need to deal with padding bytes to 256-words *)
+fun skip_padding :: "nat \<Rightarrow> nat" where
+"skip_padding n =
+  (case divmod_nat n 32 of
+    (_, 0) \<Rightarrow> n
+    | (_, rem) \<Rightarrow> n + rem)"
 
 (* TODO: consider returning a nat everywhere to make it easier to keep track
    of how much we have read, for the purposes of tuple indexing *)
@@ -397,12 +403,12 @@ where
         if length l < 32 then None
         else let sz = nat (decode_uint (take 32 l)) in
              if length l - 32 < sz then None
-             else Some (Vbytes (take sz (drop 32 l)), sz + 32)
+             else Some (Vbytes (take sz (drop 32 l)), skip_padding sz + 32)
       | Tstring \<Rightarrow> 
         if length l < 32 then None
         else let sz = nat (decode_uint (take 32 l)) in
              if length l - 32 < sz then None
-             else Some (Vstring (bytes_to_string (take sz (drop 32 l))), sz + 32)
+             else Some (Vstring (bytes_to_string (take sz (drop 32 l))), skip_padding sz + 32)
       | _ \<Rightarrow> None))"
 
 (*| "decode_dyn_nocheck_array t 0 [] = Some ([], [])"
@@ -528,6 +534,7 @@ termination decode_nocheck
   done
 
 
+
 (* head = offset at which tail can be found
    tail = encoding of dynamic object *)
 
@@ -554,5 +561,128 @@ datatype AbiValueWord =
   | Bytes "nat" "256 word"
   | Function "160 word" "32 word"
 *)
+
+(* encoder *)
+(* encode :: abi_value \<Rightarrow> 8 word list *)
+
+(* TODO: decide whether to add checks to these encoders,
+   or to do so outside *)
+fun encode_int :: "int \<Rightarrow> 8 word list" where
+"encode_int i =
+  Word.word_rsplit (Word.word_of_int i :: 256 word)"
+
+fun encode_bool :: "bool \<Rightarrow> 8 word list" where
+"encode_bool True = encode_int 1"
+| "encode_bool False = encode_int 0"
+
+fun encode_fixed :: "nat \<Rightarrow> rat \<Rightarrow> 8 word list" where
+"encode_fixed n r =
+  encode_int (fst (Rat.quotient_of (r * (10 ^ n))))"
+
+fun encode_fbytes :: "nat \<Rightarrow> 8 word list \<Rightarrow> 8 word list" where
+"encode_fbytes n l = 
+  take n l"
+
+fun encode_static :: "abi_value \<Rightarrow> 8 word list option" where
+"encode_static (Vuint n i) = Some (encode_int i)"
+| "encode_static (Vsint n i) = Some (encode_int i)"
+| "encode_static (Vaddr a) = Some (encode_int a)"
+| "encode_static (Vbool b) = Some (encode_bool b)"
+| "encode_static (Vfixed m n r) = Some (encode_fixed n r)"
+| "encode_static (Vufixed m n r) = Some (encode_fixed n r)"
+| "encode_static (Vfbytes n l) = Some (encode_fbytes n l)"
+| "encode_static (Vfarray t n l) =
+    (case List.those (List.map encode_static l) of
+      None \<Rightarrow> None
+      | Some bs \<Rightarrow> Some (List.concat bs))"
+| "encode_static (Vtuple ts vs) =
+        (case List.those (List.map encode_static vs) of
+          None \<Rightarrow> None
+         | Some bs \<Rightarrow> Some (List.concat bs))"
+| "encode_static _ = None"
+
+(* for dynamic tuples, we need to do the following:
+   - encode heads
+   - encode all values (map)
+   - encode tail *)
+
+fun string_to_bytes :: "char list \<Rightarrow> 8 word list" where
+"string_to_bytes s = 
+  List.map (\<lambda> c . word_of_int (int_of_integer (integer_of_char c))) s"
+
+(* pad a byte stream to a multiple of 32 bytes *)
+fun pad_bytes :: "8 word list \<Rightarrow> 8 word list" where
+"pad_bytes l = 
+  (case divmod_nat (length l) 32 of
+        (_, 0) \<Rightarrow> l
+        | (_, rem) \<Rightarrow> l @ (List.replicate rem (Word.word_of_int 0)))"
+
+fun heads_length :: "(8 word list + abi_value) list \<Rightarrow> nat" where
+"heads_length [] = 0"
+| "heads_length (Inl bs # t) = length bs + heads_length t"
+| "heads_length (Inr v # t) = 32 + heads_length t"
+
+fun encode :: "abi_value \<Rightarrow> 8 word list option" 
+and encode_tuple_heads :: "abi_value list \<Rightarrow> (8 word list + abi_value) list option" 
+(*
+and encode_tuple_tails :: "(8 word list + abi_value) list \<Rightarrow> nat \<Rightarrow> 8 word list \<Rightarrow> 8 word list option" where *)
+
+and encode_tuple_tails :: "(abi_value) list \<Rightarrow> 8 word list \<Rightarrow> 8 word list list" where
+"encode v =
+  (if abi_type_isstatic (abi_get_type v) then
+      encode_static v
+   else (case v of
+          Vfarray t n vs \<Rightarrow>
+          (case List.those (List.map encode vs) of
+                None \<Rightarrow> None
+                | Some bss \<Rightarrow> Some (List.concat bss))
+          | Varray t vs \<Rightarrow>
+            (case List.those (List.map encode vs) of
+                None \<Rightarrow> None
+                | Some bss \<Rightarrow> Some (encode_int (int (length vs)) @ List.concat bss))
+          | Vtuple ts vs \<Rightarrow> 
+              (case encode_tuple_heads vs of
+                None \<Rightarrow> None
+                | Some bvs \<Rightarrow>
+                  (case encode_tuple_tails bvs (heads_length bvs) [] of
+                        None \<Rightarrow> None
+                        | Some bs \<Rightarrow> Some bs))
+          | Vbytes l \<Rightarrow> Some (encode_int (length l) @ pad_bytes l)
+          | Vstring s \<Rightarrow> let bs = string_to_bytes s in
+                         Some (encode_int (length bs) @ pad_bytes bs)
+          | _ \<Rightarrow> None))"
+
+(* need to refactor for cleaner termination proof
+idea: encode tails first?
+
+*)
+| "encode_tuple_heads [] = Some []"
+| "encode_tuple_heads (v#vs) =
+    (if abi_type_isstatic (abi_get_type v) then
+        (case encode v of
+          None \<Rightarrow> None
+          | Some bs \<Rightarrow> (case (encode_tuple_heads vs) of
+                          None \<Rightarrow> None
+                          | Some bs' \<Rightarrow> Some (Inl bs # bs')))
+    else (case (encode_tuple_heads vs) of
+                None \<Rightarrow> None
+                | Some bs' \<Rightarrow> Some (Inr v # bs')))
+    "
+| "encode_tuple_tails [] n tails = Some tails"
+| "encode_tuple_tails (Inl bs # rest) n tails =
+    (case encode_tuple_tails rest n tails of
+      None \<Rightarrow> None
+      | Some bss \<Rightarrow> Some (bs @ bss))"
+| "encode_tuple_tails (Inr v # rest) n tails =
+    (case encode v of
+            None \<Rightarrow> None
+           | Some bs1 \<Rightarrow> 
+              (case encode_tuple_tails rest (n + length bs1) (tails @ bs1) of
+                  None \<Rightarrow> None
+                  | Some bss \<Rightarrow> Some ((encode_int n) @ bss)))"
+        
+          
+
+value "Word.word_of_int (-1) :: 256 word"
 
 end
