abfb4c5f3788512147be4884440d97639fdb68e2
mario.alvarez@consensys.net <yorix@DESKTOP-84OVJN9.localdomain>
2020-04-02 12:09:24 -0700

    Forgot to add OK file

---
+++ Ok.thy
@@ -0,0 +1,28 @@
+theory Ok imports Main
+
+begin
+type_synonym 'a orerror = "'a + char list"
+
+abbreviation Ok :: "'a \<Rightarrow> 'a orerror" where
+"Ok \<equiv> Inl"
+
+abbreviation Err :: "char list \<Rightarrow> 'a orerror" where
+"Err \<equiv> Inr"
+
+fun those_err :: "'a orerror list \<Rightarrow> 'a list orerror" where
+"those_err [] = Ok []"
+| "those_err ((Ok h)#t) = 
+    (case those_err t of
+      Err s \<Rightarrow> Err s
+      | Ok t' \<Rightarrow> Ok (h#t'))"
+| "those_err ((Err s)#_) = Err s"
+
+fun map_err :: "('a \<Rightarrow> 'b) \<Rightarrow> ('a orerror) \<Rightarrow> ('b orerror)" where
+"map_err f (Ok a) = (Ok (f a))"
+| "map_err f (Err s) = Err s"
+
+fun bind_err :: "('a \<Rightarrow> 'b orerror) \<Rightarrow> ('a orerror) \<Rightarrow> ('b orerror)" where
+"bind_err f (Ok a) = f a"
+| "bind_err f (Err s) = Err s"
+
+end
