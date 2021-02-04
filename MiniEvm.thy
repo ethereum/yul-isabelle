8f1fbc8059ca35387471374b260dd239e37d99d6
Mario Alvarez <mario.alvarez@mesh.xyz>
2021-02-03 17:22:49 -0800

    some work on arithmetic operations for EVM

--- MiniEvm.thy
+++ MiniEvm.thy
@@ -16,111 +16,151 @@ record estate =
   calldata :: "eint list"
 
 (* *)
+(*
 consts mkBuiltin ::
   "'x \<Rightarrow> ('v list \<Rightarrow> (('g, 'v list) State) Result)"
+*)
+
+
+
+consts mkBuiltin ::
+  "'x \<Rightarrow> ('g, 'v, 't) function_sig'"
 
 (* 1st number = number of arguments
    2nd number = number of returns *)
 
+fun mkNames :: "String.literal list \<Rightarrow> ('t YulTypedName list)"
+  where
+"mkNames [] = []"
+| "mkNames (s1 # st) =
+  YulTypedName s1 YulDefaultType # mkNames st"
+
 definition mkBuiltin_0_0 ::
 "('g, unit) State \<Rightarrow>
- ('v list \<Rightarrow> (('g, 'v list) State) Result)" where
-"mkBuiltin_0_0 f vs =
-  (case vs of
-    [] \<Rightarrow> Result 
-            (\<lambda> g . 
-              (case f g of
-                ((), g') \<Rightarrow> ([], g')))
-    | _ \<Rightarrow> Error (STR ''Argument arity error'') None)"
+ ('g, 'v, 't) function_sig'" where
+"mkBuiltin_0_0 f =
+  \<lparr> f_sig_arguments = mkNames []
+  , f_sig_returns = mkNames []
+  , f_sig_body = YulBuiltin
+    (\<lambda> vs . case vs of
+      [] \<Rightarrow> Result 
+              (\<lambda> g . 
+                (case f g of
+                  ((), g') \<Rightarrow> ([], g')))
+      | _ \<Rightarrow> Error (STR ''Argument arity error'') None) \<rparr>"
 
 definition mkBuiltin_1_0 ::
 "('v \<Rightarrow> ('g, unit) State) \<Rightarrow>
- ('v list \<Rightarrow> (('g, 'v list) State) Result)" where
-"mkBuiltin_1_0 f vs =
-  (case vs of
-    [v] \<Rightarrow> Result 
-            (\<lambda> g . 
-              (case f v g of
-                ((), g') \<Rightarrow> ([], g')))
-    | _ \<Rightarrow> Error (STR ''Argument arity error'') None)"
+ ('g, 'v, 't) function_sig'" where
+"mkBuiltin_1_0 f =
+  \<lparr> f_sig_arguments = mkNames [STR ''a1'']
+    , f_sig_returns = mkNames []
+    , f_sig_body = YulBuiltin
+      (\<lambda> vs . case vs of
+        [v] \<Rightarrow> Result 
+                (\<lambda> g . 
+                  (case f v g of
+                    ((), g') \<Rightarrow> ([], g')))
+        | _ \<Rightarrow> Error (STR ''Argument arity error'') None) \<rparr>"
 
 definition mkBuiltin_1_1 ::
 "('v \<Rightarrow> ('g, 'v) State) \<Rightarrow>
- ('v list \<Rightarrow> (('g, 'v list) State) Result)" where
-"mkBuiltin_1_1 f vs =
-  (case vs of
-    [v] \<Rightarrow> Result 
-            (\<lambda> g . 
-              (case f v g of
-                (v', g') \<Rightarrow> ([v'], g')))
-    | _ \<Rightarrow> Error (STR ''Argument arity error'') None)"
+ ('g, 'v, 't) function_sig'" where
+"mkBuiltin_1_1 f =
+  \<lparr> f_sig_arguments = mkNames [STR ''a1'']
+  , f_sig_returns = mkNames [STR ''r1'']
+  , f_sig_body = YulBuiltin
+    (\<lambda> vs . (case vs of
+      [v] \<Rightarrow> Result 
+              (\<lambda> g . 
+                (case f v g of
+                  (v', g') \<Rightarrow> ([v'], g')))
+      | _ \<Rightarrow> Error (STR ''Argument arity error'') None))\<rparr>"
 
 definition mkBuiltin_2_0 ::
 "('v \<Rightarrow> 'v \<Rightarrow> ('g, unit) State) \<Rightarrow>
- ('v list \<Rightarrow> (('g, 'v list) State) Result)" where
-"mkBuiltin_2_0 f vs =
-  (case vs of
-    [v1, v2] \<Rightarrow> Result 
-            (\<lambda> g . 
-              (case f v1 v2 g of
-                ((), g') \<Rightarrow> ([], g')))
-    | _ \<Rightarrow> Error (STR ''Argument arity error'') None)"
+ ('g, 'v, 't) function_sig'" where
+"mkBuiltin_2_0 f =
+  \<lparr> f_sig_arguments = mkNames [STR ''a1'', STR ''a2'']
+  , f_sig_returns = mkNames []
+  , f_sig_body = YulBuiltin
+    (\<lambda> vs . (case vs of
+      [v1, v2] \<Rightarrow> Result 
+              (\<lambda> g . 
+                (case f v1 v2 g of
+                  ((), g') \<Rightarrow> ([], g')))
+      | _ \<Rightarrow> Error (STR ''Argument arity error'') None))\<rparr>"
 
 definition mkBuiltin_2_1 ::
 "('v \<Rightarrow> 'v \<Rightarrow> ('g, 'v) State) \<Rightarrow>
- ('v list \<Rightarrow> (('g, 'v list) State) Result)" where
-"mkBuiltin_2_1 f vs =
-  (case vs of
-    [v1, v2] \<Rightarrow> Result 
-            (\<lambda> g . 
-              (case f v1 v2 g of
-                (v', g') \<Rightarrow> ([v'], g')))
-    | _ \<Rightarrow> Error (STR ''Argument arity error'') None)"
+ ('g, 'v, 't) function_sig'" where
+"mkBuiltin_2_1 f =
+  \<lparr> f_sig_arguments = mkNames [STR ''a1'', STR ''a2'']
+  , f_sig_returns = mkNames [STR ''r1'']
+  , f_sig_body = YulBuiltin
+    (\<lambda> vs . (case vs of
+      [v1, v2] \<Rightarrow> Result 
+              (\<lambda> g . 
+                (case f v1 v2 g of
+                  (v', g') \<Rightarrow> ([v'], g')))
+      | _ \<Rightarrow> Error (STR ''Argument arity error'') None))\<rparr>"
 
 definition mkBuiltin_2_2 ::
 "('v \<Rightarrow> 'v \<Rightarrow> ('g, ('v * 'v)) State) \<Rightarrow>
- ('v list \<Rightarrow> (('g, 'v list) State) Result)" where
-"mkBuiltin_2_2 f vs =
-  (case vs of
-    [v1, v2] \<Rightarrow> Result 
-            (\<lambda> g . 
-              (case f v1 v2 g of
-                ((v'1, v'2), g') \<Rightarrow> ([v'1, v'2], g')))
-    | _ \<Rightarrow> Error (STR ''Argument arity error'') None)"
+ ('g, 'v, 't) function_sig'" where
+"mkBuiltin_2_2 f =
+  \<lparr> f_sig_arguments = mkNames [STR ''a1'', STR ''a2'']
+  , f_sig_returns = mkNames [STR ''r1'', STR ''r2'']
+  , f_sig_body = YulBuiltin
+    (\<lambda> vs . (case vs of
+      [v1, v2] \<Rightarrow> Result 
+              (\<lambda> g . 
+                (case f v1 v2 g of
+                  ((v'1, v'2), g') \<Rightarrow> ([v'1, v'2], g')))
+      | _ \<Rightarrow> Error (STR ''Argument arity error'') None)) \<rparr>"
 
 
 definition mkBuiltin_3_0 ::
 "('v \<Rightarrow> 'v \<Rightarrow> 'v \<Rightarrow> ('g, unit) State) \<Rightarrow>
- ('v list \<Rightarrow> (('g, 'v list) State) Result)" where
-"mkBuiltin_3_0 f vs =
-  (case vs of
-    [v1, v2, v3] \<Rightarrow> Result 
-            (\<lambda> g . 
-              (case f v1 v2 v3 g of
-                ((), g') \<Rightarrow> ([], g')))
-    | _ \<Rightarrow> Error (STR ''Argument arity error'') None)"
+ ('g, 'v, 't) function_sig'" where
+"mkBuiltin_3_0 f =
+  \<lparr> f_sig_arguments = mkNames [STR ''a1'', STR ''a2'', STR ''a3'']
+  , f_sig_returns = mkNames []
+  , f_sig_body = YulBuiltin
+    (\<lambda> vs . (case vs of
+      [v1, v2, v3] \<Rightarrow> Result 
+              (\<lambda> g . 
+                (case f v1 v2 v3 g of
+                  ((), g') \<Rightarrow> ([], g')))
+      | _ \<Rightarrow> Error (STR ''Argument arity error'') None)) \<rparr>"
 
 definition mkBuiltin_3_1 ::
 "('v \<Rightarrow> 'v \<Rightarrow> 'v \<Rightarrow> ('g, 'v) State) \<Rightarrow>
- ('v list \<Rightarrow> (('g, 'v list) State) Result)" where
-"mkBuiltin_3_1 f vs =
-  (case vs of
-    [v1, v2, v3] \<Rightarrow> Result 
-            (\<lambda> g . 
-              (case f v1 v2 v3 g of
-                (v', g') \<Rightarrow> ([v'], g')))
-    | _ \<Rightarrow> Error (STR ''Argument arity error'') None)"
+ ('g, 'v, 't) function_sig'" where
+"mkBuiltin_3_1 f =
+  \<lparr> f_sig_arguments = mkNames [STR ''a1'', STR ''a2'', STR ''a3'']
+  , f_sig_returns = mkNames [STR ''r1'']
+  , f_sig_body = YulBuiltin
+    (\<lambda> vs . (case vs of
+      [v1, v2, v3] \<Rightarrow> Result 
+              (\<lambda> g . 
+                (case f v1 v2 v3 g of
+                  (v', g') \<Rightarrow> ([v'], g')))
+      | _ \<Rightarrow> Error (STR ''Argument arity error'') None)) \<rparr>"
 
 definition mkBuiltin_3_2 ::
 "('v \<Rightarrow> 'v \<Rightarrow> 'v \<Rightarrow> ('g, ('v * 'v)) State) \<Rightarrow>
- ('v list \<Rightarrow> (('g, 'v list) State) Result)" where
-"mkBuiltin_3_2 f vs =
-  (case vs of
-    [v1, v2, v3] \<Rightarrow> Result 
-            (\<lambda> g . 
-              (case f v1 v2 v3 g of
-                ((v'1, v'2), g') \<Rightarrow> ([v'1, v'2], g')))
-    | _ \<Rightarrow> Error (STR ''Argument arity error'') None)"
+ ('g, 'v, 't) function_sig'" where
+"mkBuiltin_3_2 f =
+  \<lparr> f_sig_arguments = mkNames [STR ''a1'', STR ''a2'', STR ''a3'']
+  , f_sig_returns = mkNames [STR ''r1'', STR ''r2'']
+  , f_sig_body = YulBuiltin
+    (\<lambda> vs . (case vs of
+      [v1, v2, v3] \<Rightarrow> Result 
+              (\<lambda> g . 
+                (case f v1 v2 v3 g of
+                  ((v'1, v'2), g') \<Rightarrow> ([v'1, v'2], g')))
+      | _ \<Rightarrow> Error (STR ''Argument arity error'') None)) \<rparr>"
 
 adhoc_overloading mkBuiltin
   mkBuiltin_0_0
@@ -133,4 +173,95 @@ adhoc_overloading mkBuiltin
   mkBuiltin_3_1
   mkBuiltin_3_2
 
+(*
+ * EVM instruction semantics
+ *)
+(*
+	STOP = 0x00,		///< halts execution
+	ADD,				///< addition operation
+	MUL,				///< multiplication operation
+	SUB,				///< subtraction operation
+	DIV,				///< integer division operation
+	SDIV,				///< signed integer division operation
+	MOD,				///< modulo remainder operation
+	SMOD,				///< signed modulo remainder operation
+	ADDMOD,				///< unsigned modular addition
+	MULMOD,				///< unsigned modular multiplication
+	EXP,				///< exponential operation
+	SIGNEXTEND,			///< extend length of signed integer
+*)
+
+fun stop :: "(estate, unit) State" where
+"stop s = ((), s \<lparr> flag := Halt \<rparr>)"
+
+(* TODO: Eth-Isabelle does not directly use HOL-Word primitives for arithmetic
+   operations - instead it converts to integers, does integer operations, and then converts back.
+   Using the library primitives feels cleaner, but we should make sure the semantics are the same. *)
+fun add :: "eint \<Rightarrow> eint \<Rightarrow> (estate, eint) State" where
+"add i1 i2 s = (plus_word_inst.plus_word i1 i2, s)"
+
+fun mul :: "eint \<Rightarrow> eint \<Rightarrow> (estate, eint) State" where
+"mul i1 i2 s = (times_word_inst.times_word i1 i2, s)"
+
+fun sub :: "eint \<Rightarrow> eint \<Rightarrow> (estate, eint) State" where
+"sub i1 i2 s = (minus_word_inst.minus_word i1 i2, s)"
+
+fun divd :: "eint \<Rightarrow> eint \<Rightarrow> (estate, eint) State" where
+"divd i1 i2 s = (divide_word_inst.divide_word i1 i2, s)"
+
+(* get minimum-valued word (2's complement) *)
+definition ssmallest :: "('a :: len0) word" where
+"ssmallest =
+ (Word.setBit (Word.word_of_int 0 :: 'a word) (size (Word.word_of_int 0 :: 'a word) - 1))"
+
+(* absolute value - NB this does not work for minimum signed value*)
+fun word_abs :: "('a :: len) word \<Rightarrow> 'a word" where
+"word_abs w =
+  (if sint w < 0 then times_word_inst.times_word (word_of_int (-1)) w else w)"
+
+value "sint (times_word_inst.times_word (word_of_int (-1)) (word_abs (word_of_int (-126)) :: 8 word))"
+
+(* get sign bit *)
+
+(* helper for signed division.
+   TODO: double check this is what we want *)
+fun sdivd' :: "('a :: len) word \<Rightarrow> 'a word \<Rightarrow> 'a word" where
+"sdivd' i1 i2 = 
+  (if (i1 = ssmallest \<and> i2 = Word.word_of_int(-1))
+   then ssmallest
+   else 
+   (case ((sint i1 < 0), (sint i2 < 0)) of
+    (True, True) \<Rightarrow>
+      times_word_inst.times_word (word_of_int (-1)) 
+                                 (divide_word_inst.divide_word (word_abs i1) (word_abs i2))
+    | (False, False) \<Rightarrow>
+      times_word_inst.times_word (word_of_int (-1)) 
+                                 (divide_word_inst.divide_word (word_abs i1) (word_abs i2))
+    | _ \<Rightarrow> (divide_word_inst.divide_word (word_abs i1) (word_abs i2))))"
+
+fun sdivd :: "eint \<Rightarrow> eint \<Rightarrow> (estate, eint) State" where
+"sdivd i1 i2 s = (sdivd' i1 i2, s)"
+
+fun modu' :: "('a :: len) word \<Rightarrow> 'a word \<Rightarrow> 'a word" where
+"modu' i1 i2 =
+  (if i2 = word_of_int 0 then word_of_int 0
+   else modulo_word_inst.modulo_word i1 i2)"
+
+fun modu :: "eint \<Rightarrow> eint \<Rightarrow> (estate, eint) State" where
+"modu i1 i2 s = (modu' i1 i2, s)"
+
+fun smodu' :: "('a :: len) word \<Rightarrow> 'a word \<Rightarrow> 'a word" where
+"smodu' i1 i2 =
+  (if sint i1 < 0 then
+    times_word_inst.times_word (word_of_int (-1)) (modu' (word_abs i1) (word_abs i2))
+   else modu' (word_abs i1) (word_abs i2))"
+
+fun smodu :: "eint \<Rightarrow> eint \<Rightarrow> (estate, eint) State" where
+"smodu i1 i2 s = 
+  (smodu' i1 i2, s)"
+
+(* addmod - these need to be converted out of word, since intermediate results not modded to 2^256*)
+(* mulmod - these need to be converted out of word, since intermediate results not modded to 2^256*)
+(* exp *)
+(* signextend *)
 end
