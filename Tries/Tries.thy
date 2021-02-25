5e8eac81fb6ef93e388f07b95e6ce0d7632e849a
Mario Alvarez <mario.alvarez@mesh.xyz>
2021-02-25 01:29:35 -0800

    more work on Tries; admitted instance proof for string keys

--- Tries/Tries.thy
+++ Tries/Tries.thy
@@ -370,6 +370,7 @@ proof(induction "rx_size k" arbitrary: k k' t v rule: nat_less_induct)
   qed
 qed
 
+(* instances: nat, word, string literal *)
 
 fun rx_divmod_nat ::
   "nat \<Rightarrow> (nat * bool) option" where
@@ -473,7 +474,92 @@ lemma rx_divmod_word_distinct :
   fixes w1 w2 :: "('a :: len) word"
   assumes H : "rx_divmod_word w1 = rx_divmod_word w2"
   shows "w1 = w2" using assms
-  sorry
+proof(cases "w1 = 0")
+  case True
+  show ?thesis 
+  proof(cases "w2 = 0")
+    case True' : True
+    then show ?thesis using True by auto
+  next
+    case False' : False
+    then show ?thesis using H True by(auto)
+  qed
+next
+  case False
+  show ?thesis 
+  proof(cases "w2 = 0")
+    case True' : True
+    then show ?thesis using H False
+      by(auto)
+  next
+    case False' : False
+    then show ?thesis 
+    proof(cases "uint w1 mod 2 = 1")
+      case True'' : True
+
+      have Eq : "(word_of_int (uint w1 div 2) :: 'a word) = word_of_int (uint w2 div 2)"
+        using False False' H True''
+        by(auto)
+
+      have Lt1 : "uint w1 < 2 ^ LENGTH('a)"
+        using uint_lt[of w1] by auto
+
+      hence Lt1' : "uint w1 div 2 < 2 ^ LENGTH('a)"
+        using uint_0[of w1] by(arith)
+
+      have Lt2 : "uint w2 < 2 ^ LENGTH('a)"
+        using uint_lt[of w2] by auto
+
+      hence Lt2' : "uint w2 div 2 < 2 ^ LENGTH('a)"
+        using uint_0[of w2] by(arith)
+
+      have Ge1 : "0 \<le> uint w1 div 2" using uint_0[of w1]
+        by arith
+
+      have Ge2 : "0 \<le> uint w2 div 2" using uint_0[of w2]
+        by arith
+
+      have "uint w1 div 2 = uint w2 div 2" 
+        using arg_cong[OF Eq, of uint] 
+           uint_0[of "w2 div 2"] uint_0[of "w1 div 2"]
+          int_mod_eq'[OF Ge1 Lt1'] int_mod_eq'[OF Ge2  Lt2']
+        by(simp add: uint_word_of_int)
+
+      then show ?thesis using H False False' True''
+          Misc_Arithmetic.dme[of "uint w1" 2]
+          Misc_Arithmetic.dme[of "uint w2" 2]
+        by(auto)
+    next
+      case False'' : False
+      then obtain q qa where
+        Q: "uint w1 = 2 * q" "uint w2 = 2 * qa" and
+           Qeq : "(word_of_int q :: 'a word) = word_of_int qa"
+        using False False' H by(auto) 
+
+      have Qgt0 : "0 \<le> q"
+        using Word.uint_range'[of w1] Q by auto
+
+      have QLt : "q < 2 ^ LENGTH('a)" using uint_lt[of w1] Q Qgt0
+        by(auto)
+
+      have Qagt0 : "0 \<le> qa"
+        using Word.uint_range'[of w2] Q by auto
+
+      have QaLt : "qa < 2 ^ LENGTH('a)" using uint_lt[of w2] Q Qagt0
+        by(auto)
+
+      have Qeq' : "q = qa"
+        using False False' H Q Qeq 
+        unfolding word.abs_eq_iff no_bintr_alt1
+          using int_mod_eq'[OF Qgt0 QLt] int_mod_eq'[OF Qagt0 QaLt]
+          by(auto)
+
+      show ?thesis using False False' H Q Qeq word_uint_eqI[of w1 w2]
+        unfolding word.abs_eq_iff no_bintr_alt1 Qeq'
+        by(auto)
+    qed
+  qed
+qed
 
 
 instantiation word :: (len) rx_key begin
@@ -551,7 +637,78 @@ next
     qed
   qed
 qed
-end    
+end      
+
+type_synonym partial_char =
+    "(bool *
+     (bool *
+     (bool *
+     (bool *
+     (bool *
+     (bool *
+      bool option) option) option) option) option) option) option"
+
+fun partial_char_sz :: "partial_char \<Rightarrow> nat" where
+"partial_char_sz None = 0"
+| "partial_char_sz (Some (_, None)) = 1"
+| "partial_char_sz (Some (_, Some (_, None))) = 2"
+| "partial_char_sz (Some (_, Some (_, Some (_, None)))) = 3"
+| "partial_char_sz (Some (_, Some (_, Some (_, Some (_, None))))) = 4"
+| "partial_char_sz (Some (_, Some (_, Some (_, Some (_, Some (_, None)))))) = 5"
+| "partial_char_sz (Some (_, Some (_, Some (_, Some (_, Some (_, Some (_, None))))))) = 6"
+| "partial_char_sz (Some (_, Some (_, Some (_, Some (_, Some (_, Some (_, Some _))))))) = 7"
+
+fun partial_char_split :: "char \<Rightarrow> partial_char * bool" where
+"partial_char_split c =
+ (Some (digit1 c, Some (digit2 c, Some (digit3 c, Some (digit4 c, 
+  Some (digit5 c, Some (digit6 c, Some (digit7 c))))))), digit0 c)"
+
+fun partial_char_divmod ::
+  "partial_char \<Rightarrow> (partial_char * bool) option" where
+"partial_char_divmod None = None"
+| "partial_char_divmod (Some (b, None)) = Some (None, b)"
+| "partial_char_divmod 
+  (Some (b0, Some (b1, None))) =
+  Some (Some (b1, None), b0)"
+| "partial_char_divmod
+  (Some (b0, Some (b1, Some (b2, None)))) =
+   Some (Some (b1, Some (b2, None)), b0)"
+| "partial_char_divmod
+  (Some (b0, Some (b1, Some (b2, Some (b3, None))))) =
+   Some (Some (b1, Some (b2, Some (b3, None))), b0)"
+| "partial_char_divmod
+  (Some (b0, Some (b1, Some (b2, Some (b3, Some (b4, None)))))) =
+   Some (Some (b1, Some (b2, Some (b3, Some (b4, None)))), b0)"
+| "partial_char_divmod
+  (Some (b0, Some (b1, Some (b2, Some (b3, Some (b4, Some (b5, None))))))) =
+   Some (Some (b1, Some (b2, Some (b3, Some (b4, Some (b5, None))))), b0)"
+| "partial_char_divmod
+  (Some (b0, Some (b1, Some (b2, Some (b3, Some (b4, Some (b5, Some (b6)))))))) =
+  Some (Some (b1, Some (b2, Some (b3, Some (b4, Some (b5, Some (b6, None)))))), b0)"
+
+datatype str' =
+  S "char list" 
+    "partial_char"
+
+definition str'_emp :: "str'" where
+"str'_emp = S [] None"
+
+fun str'_size :: "str' \<Rightarrow> nat" where
+"str'_size (S l pc) = 8 * length l + partial_char_sz pc"
+
+fun str'_divmod :: "str' \<Rightarrow> (str' * bool) option" where
+"str'_divmod (S [] None) = None"
+| "str'_divmod (S (ch#ct) None) =
+   (case partial_char_split ch of
+    (pc, b) \<Rightarrow> Some (S ct pc, b))"
+| "str'_divmod (S t (Some (X))) =
+  (case partial_char_divmod (Some X) of
+   None \<Rightarrow> None \<comment> \<open>bogus\<close>
+   | Some (pc', b) \<Rightarrow> Some (S t pc', b))"
+
+fun str'_of_lit :: "String.literal \<Rightarrow> str'" where
+"str'_of_lit l =
+  (S (String.explode l) None)"
 
 (* TODO: instance for string keys *)
 (*
