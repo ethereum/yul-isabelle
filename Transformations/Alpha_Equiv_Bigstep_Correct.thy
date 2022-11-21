072136e7deae211680b65c4ce69c362fc7172b3c
Mario Alvarez <mario.alvarez@mesh.xyz>
2022-11-21 09:43:32 -0800

    A bit of cleanup on attempted alpha equivalence proofs

--- Transformations/Alpha_Equiv_Bigstep_Correct.thy
+++ Transformations/Alpha_Equiv_Bigstep_Correct.thy
@@ -9,7 +9,7 @@ begin
 
 fun alpha_equiv_Result ::
   "Result \<Rightarrow> Result \<Rightarrow> bool" where
-"alpha_equiv_Result Sucess Success = True"
+"alpha_equiv_Result Success Success = True"
 | "alpha_equiv_Result (Error _) (Error _) = True"
 | "alpha_equiv_Result OutOfGas OutOfGas = True"
 | "alpha_equiv_Result _ _ = False"
@@ -52,6 +52,20 @@ type_synonym FunctionContext =
 type_synonym 'g BuiltinContext =
   \<open>Identifier \<Rightarrow> ('g BuiltinFunction) option\<close>
 
+definition alpha_equiv_function_context_elem ::
+  "(Identifier\<times> (Statement \<times> Identifier list)) \<Rightarrow> (Identifier\<times> (Statement \<times> Identifier list)) \<Rightarrow> bool" where
+"alpha_equiv_function_context_elem fe1 fe2 =
+  (case fe1 of
+    (n1, FunctionDefinition n1' params1 rets1 body1, ids1) \<Rightarrow>
+    (case fe2 of
+      (n2, FunctionDefinition n2' params2 rets2 body2, ids2) \<Rightarrow>
+        (n1 = n1' \<and> n2 = n2' \<and>
+        alpha_equiv_statement (zip ids1 ids2) []
+          (FunctionDefinition n1' params1 rets1 body1)
+          (FunctionDefinition n2' params2 rets2 body2))        
+    | _ \<Rightarrow> False)
+  | _ \<Rightarrow> False)" 
+
 (* TODO: make sure that the way we are making use of the "visible"
  * identifiers is correct.
  *)
@@ -60,17 +74,7 @@ definition alpha_equiv_function_contexts ::
 "alpha_equiv_function_contexts subst vm1 vm2 =
   (subst = zip (map fst vm1) (map fst vm2) \<and>
    list_all2
-    (\<lambda> (n1, f1) (n2, f2) .
-      (case f1 of
-        (FunctionDefinition n1' params1 rets1 body1, ids1) \<Rightarrow>
-        (case f2 of
-          (FunctionDefinition n2' params2 rets2 body2, ids2) \<Rightarrow>
-            (n1 = n1' \<and> n2 = n2' \<and>
-            alpha_equiv_statement (zip ids1 ids2) []
-              (FunctionDefinition n1' params1 rets1 body1)
-              (FunctionDefinition n2' params2 rets2 body2))        
-        | _ \<Rightarrow> False)
-      | _ \<Rightarrow> False)) vm1 vm2)"
+    alpha_equiv_function_context_elem vm1 vm2)"
 
 definition alpha_equiv_builtin_contexts ::
    "(name * name) list \<Rightarrow> 'g BuiltinContext \<Rightarrow> 'g BuiltinContext \<Rightarrow> bool" where
@@ -136,6 +140,25 @@ lemma find_map_of :
   "map_option snd (find (\<lambda> (declaredName,_) . declaredName = idn) l) =
    map_of l idn"
 proof(induction l arbitrary: idn)
+  case Nil
+  then show ?case
+    by(auto)
+next
+  case (Cons lh lt)
+
+  obtain lhk lhv where Lh : "lh = (lhk, lhv)"
+    by(cases lh; auto)
+
+  show ?case using Cons Lh
+    by(auto)
+qed
+
+lemma find_map_of_alt :
+  "find (\<lambda>(name, _). name = k) l = 
+   (case map_of l k of
+    None \<Rightarrow> None
+    | Some v \<Rightarrow> Some (k, v))"
+proof(induction l arbitrary: k)
   case Nil
   then show ?case
     by(auto)
@@ -263,6 +286,149 @@ next
       then show ?thesis using Cons Substh Neq1
         by(auto simp add: alpha_equiv_name_def flip_def)
     qed
+  qed
+qed
+
+
+fun unpack_FunctionDefinition ::
+  "Statement \<Rightarrow> (Identifier * Identifier list * Identifier list * Statement list) option" where
+"unpack_FunctionDefinition (FunctionDefinition name params rets body) = Some (name, params, rets, body)"
+| "unpack_FunctionDefinition _ = None"
+
+lemma unpack_FunctionDefinition_case :
+  "(case st of
+    FunctionDefinition name params rets body \<Rightarrow> f1 name params rets body
+    | _ \<Rightarrow> f2) =
+   (case unpack_FunctionDefinition st of
+    Some (name, params, rets, body) \<Rightarrow> f1 name params rets body
+    | None \<Rightarrow> f2)"
+  by(cases st; auto)
+
+lemma list_all2_equiv_name_function_contexts :
+  assumes "alpha_equiv_name fsubst f1 f2"
+  assumes "alpha_equiv_function_contexts fsubst fc1 fc2"
+  assumes "map_of fc1 f1 = Some x1"
+  assumes "map_of fc2 f2 = Some x2"
+  assumes "list_all2 P fc1 fc2"
+  shows "P (f1, x1) (f2, x2)"
+  using assms
+proof(induction fc1 arbitrary: fsubst fc2 f1 f2 x1 x2)
+  case Nil
+  then show ?case 
+    by(auto)
+next
+  case Cons1 : (Cons fc1h fc1t)
+
+  then obtain fc2h fc2t where Cons2 :
+    "fc2 = fc2h # fc2t"
+    by(cases fc2; auto)
+
+  obtain fc1k fc1v where Fc1h : "fc1h = (fc1k, fc1v)"
+    by(cases fc1h; auto)
+
+  obtain fc2k fc2v where Fc2h : "fc2h = (fc2k, fc2v)"
+    by(cases fc2h; auto)
+
+  obtain fsubsth fsubstt where Cons_f :
+    "fsubst = fsubsth # fsubstt"
+    using Cons1.prems
+    by(cases fsubst; auto simp add: alpha_equiv_function_contexts_def)
+
+  show ?case
+  proof(cases "f1 = fc1k")
+    case Eq1 : True
+
+    then have Eq2 : "f2 = fc2k"
+      using Cons1.prems Cons2 Fc1h Fc2h
+      by(auto simp add: alpha_equiv_name_def alpha_equiv_function_contexts_def flip_def split: option.splits)
+
+    then show ?thesis
+      using Cons1.prems Cons2 Fc1h Fc2h
+      by(auto simp add: alpha_equiv_name_def alpha_equiv_function_contexts_def flip_def split: option.splits)
+  next
+    case Neq1 : False
+
+    then have Neq2 : "f2 \<noteq> fc2k"
+      using Cons1.prems Cons2 Fc1h Fc2h
+      by(auto simp add: alpha_equiv_name_def alpha_equiv_function_contexts_def flip_def split: option.splits)
+
+    have Contexts_tl : "alpha_equiv_function_contexts fsubstt fc1t fc2t"
+      using Cons1.prems Cons2 Cons_f Fc1h Fc2h Neq1 Neq2
+      by(auto simp add: alpha_equiv_function_contexts_def alpha_equiv_function_context_elem_def)
+
+    have Name' : "alpha_equiv_name fsubstt f1 f2"
+      using Neq1 Neq2 Cons1.prems Cons2 Fc1h Fc2h Cons_f
+      by(auto simp add: alpha_equiv_name_def alpha_equiv_function_contexts_def flip_def
+          split: option.splits)
+
+    show ?thesis
+      using Cons1.prems Cons2 Fc1h Fc2h Neq1 Neq2
+      using Cons1.IH[OF Name' Contexts_tl]
+      by(auto simp add: alpha_equiv_name_def alpha_equiv_function_contexts_def flip_def split: option.splits)
+  qed
+qed
+
+lemma list_all2_equiv_name_local_variables :
+  assumes "alpha_equiv_name subst f1 f2"
+  assumes "alpha_equiv_local_variables subst fc1 fc2"
+  assumes "map_of fc1 f1 = Some x1"
+  assumes "map_of fc2 f2 = Some x2"
+  assumes "list_all2 P fc1 fc2"
+  shows "P (f1, x1) (f2, x2)"
+  using assms
+proof(induction fc1 arbitrary: subst fc2 f1 f2 x1 x2)
+  case Nil
+  then show ?case 
+    by(auto)
+next
+  case Cons1 : (Cons fc1h fc1t)
+
+  then obtain fc2h fc2t where Cons2 :
+    "fc2 = fc2h # fc2t"
+    by(cases fc2; auto)
+
+  obtain fc1k fc1v where Fc1h : "fc1h = (fc1k, fc1v)"
+    by(cases fc1h; auto)
+
+  obtain fc2k fc2v where Fc2h : "fc2h = (fc2k, fc2v)"
+    by(cases fc2h; auto)
+
+  obtain substh substt where Cons_f :
+    "subst = substh # substt"
+    using Cons1.prems
+    by(cases subst; auto simp add: alpha_equiv_local_variables_def)
+
+  show ?case
+  proof(cases "f1 = fc1k")
+    case Eq1 : True
+
+    then have Eq2 : "f2 = fc2k"
+      using Cons1.prems Cons2 Fc1h Fc2h
+      by(auto simp add: alpha_equiv_name_def alpha_equiv_local_variables_def flip_def split: option.splits)
+
+    then show ?thesis
+      using Cons1.prems Cons2 Fc1h Fc2h
+      by(auto simp add: alpha_equiv_name_def alpha_equiv_local_variables_def flip_def split: option.splits)
+  next
+    case Neq1 : False
+
+    then have Neq2 : "f2 \<noteq> fc2k"
+      using Cons1.prems Cons2 Fc1h Fc2h
+      by(auto simp add: alpha_equiv_name_def alpha_equiv_local_variables_def flip_def split: option.splits)
+
+    have Contexts_tl : "alpha_equiv_local_variables substt fc1t fc2t"
+      using Cons1.prems Cons2 Cons_f Fc1h Fc2h Neq1 Neq2
+      by(auto simp add: alpha_equiv_local_variables_def alpha_equiv_function_context_elem_def)
+
+    have Name' : "alpha_equiv_name substt f1 f2"
+      using Neq1 Neq2 Cons1.prems Cons2 Fc1h Fc2h Cons_f
+      by(auto simp add: alpha_equiv_name_def alpha_equiv_local_variables_def flip_def
+          split: option.splits)
+
+    show ?thesis
+      using Cons1.prems Cons2 Fc1h Fc2h Neq1 Neq2
+      using Cons1.IH[OF Name' Contexts_tl]
+      by(auto simp add: alpha_equiv_name_def alpha_equiv_function_contexts_def flip_def split: option.splits)
   qed
 qed
 
@@ -518,6 +684,16 @@ next
       obtain bivals2_o biresc2 where Builtin2 : "run_state (BuiltinFunctionOfIdentifier f2) argsc2 = (bivals2_o, biresc2)"
         by force
 
+      have Biresc_eq_argsc : "biresc1 = argsc1" "biresc2 = argsc2"
+        using Builtin1 Builtin2
+        by(auto simp add: BuiltinFunctionOfIdentifier_def split: option.splits)
+
+      have Equiv_after_get_builtin : "alpha_equiv_yulcontext fsubst vsubst biresc1 biresc2"
+        using Equiv_after_args Builtin1 Builtin2
+        by(auto simp add: BuiltinFunctionOfIdentifier_def split: option.splits)
+
+(* we should probably show biresc1 and biresc2 are also equal. *)
+
       show ?thesis
       proof(cases bivals1_o)
         case Isnt_builtin1 : None
@@ -539,30 +715,175 @@ next
           using Hexpr Hctx Hrun1 Hrun2 Fc1 Fc2 Args1 Args2 Builtin1 Builtin2 Isnt_builtin1 Isnt_builtin1' Isnt_builtin2'
           by(cases bivals2_o; auto simp add: BuiltinFunctionOfIdentifier_def)
 
-        show ?thesis using Hexpr Hctx Hrun1 Hrun2 Fc1 Fc2 Args1 Args2 Builtin1 Builtin2 Isnt_builtin1' Isnt_builtin2'
-          apply(auto simp add: alpha_equiv_yulcontext_def alpha_equiv_function_contexts_def BuiltinFunctionOfIdentifier_def)
+        obtain fundef1 fundefsc1 where Fundef1 : "run_state (FunctionDefinitionOfIdentifier f1) biresc1 = (fundef1, fundefsc1)"
+          by(cases "run_state (FunctionDefinitionOfIdentifier f1) biresc1"; auto)
 
-(*
-        then have Isnt_builtin2' : "BuiltinFunctions argsc2 f2 = None"
-          using Equiv_after_args Hexpr Fc1 Fc2
-          apply(auto simp add: alpha_equiv_yulcontext_def alpha_equiv_function_contexts)
+        obtain fundef2 fundefsc2 where Fundef2 : "run_state (FunctionDefinitionOfIdentifier f2) biresc2 = (fundef2, fundefsc2)"
+          by(cases "run_state (FunctionDefinitionOfIdentifier f2) biresc2"; auto)
 
-        have Isnt_builtin2 : "bivals2_o = None"
-          using Hexpr Hctx Hrun1 Hrun2 Fc1 Fc2 Args1 Args2 Builtin1 Builtin2 Isnt_builtin1 Isnt_builtin1'
-          apply(auto simp add: alpha_equiv_yulcontext_def alpha_equiv_function_contexts_def BuiltinFunctionOfIdentifier_def)
+        obtain fundef1_def fundef1_decls where Fundef1' :
+          "fundef1 = (fundef1_def, fundef1_decls)"
+          by force
 
+        obtain fundef2_def fundef2_decls where Fundef2' :
+          "fundef2 = (fundef2_def, fundef2_decls)"
+          by force
+
+        have F1_f2_in_functions : "(f1, f2) \<in> set (zip (map fst (Functions biresc1)) (map fst (Functions biresc2)))"
+          using Equiv_after_get_builtin alpha_equiv_name_In[OF Fname]
+          by(auto simp add: alpha_equiv_yulcontext_def alpha_equiv_function_contexts_def BuiltinFunctionOfIdentifier_def FunctionDefinitionOfIdentifier_def find_map_of_alt unpack_FunctionDefinition_case errorState_def alpha_equiv_name_def)
+
+        obtain f1body' where 
+          "(f1, f1body') \<in> set (Functions biresc1)"
+          using set_zip_leftD[OF F1_f2_in_functions]
+          by fastforce
+
+        then obtain f1body where F1body : "map_of (Functions biresc1) f1 = Some f1body"
+          using weak_map_of_SomeI
+          by fast
+
+        obtain f2body' where 
+          "(f2, f2body') \<in> set (Functions biresc2)"
+          using set_zip_rightD[OF F1_f2_in_functions]
+          by fastforce
+
+        then obtain f2body where F2body : "map_of (Functions biresc2) f2 = Some f2body"
+          using weak_map_of_SomeI
+          by fast
+
+        have Fundefsc_eq_biresc : "fundefsc1 = biresc1" "fundefsc2 = biresc2"
+          using Fundef1 Fundef2 F1body F2body
+          by( auto simp add: FunctionDefinitionOfIdentifier_def find_map_of_alt split: option.splits)
+
+        have Equiv_after_fundef :
+          "alpha_equiv_yulcontext fsubst vsubst fundefsc1 fundefsc2"
+          using Fundef1 Fundef2 Equiv_after_get_builtin F1body F2body
+          by(auto simp add: FunctionDefinitionOfIdentifier_def find_map_of_alt)
+
+        have Elems_after_fundef : "list_all2 alpha_equiv_function_context_elem (Functions fundefsc1) (Functions fundefsc2)"
+          using Equiv_after_fundef Fundef1 Fundef1' Fundef2 Fundef2' F1body F2body
+          by(auto simp add: alpha_equiv_yulcontext_def alpha_equiv_function_contexts_def BuiltinFunctionOfIdentifier_def FunctionDefinitionOfIdentifier_def find_map_of_alt unpack_FunctionDefinition_case errorState_def)
+
+        have Elem_after_fundef : "alpha_equiv_function_context_elem (f1, f1body) (f2, f2body)"
+          using Elems_after_fundef F1body F2body Equiv_after_fundef
+          using list_all2_equiv_name_function_contexts[OF Fname _ _ _ Elems_after_fundef]
+          unfolding Fundefsc_eq_biresc
+          by(auto simp add: alpha_equiv_yulcontext_def alpha_equiv_function_contexts_def BuiltinFunctionOfIdentifier_def FunctionDefinitionOfIdentifier_def find_map_of_alt unpack_FunctionDefinition_case errorState_def)
+
+        have F1body_eq : "f1body = fundef1"
+          using Elem_after_fundef Fundef1 Fundef1' Fundefsc_eq_biresc F1body
+          by(auto simp add: FunctionDefinitionOfIdentifier_def find_map_of_alt)
+
+        have F2body_eq : "f2body = fundef2"
+          using Elem_after_fundef Fundef2 Fundef2' Fundefsc_eq_biresc F2body
+          by(auto simp add: FunctionDefinitionOfIdentifier_def find_map_of_alt)
+
+        (* next, check to see if we found a valid function *)
         show ?thesis
-          using Hexpr Hctx Hrun1 Hrun2 Fc1 Fc2 Args1 Args2 Builtin1 Builtin2 Isnt_builtin1
-          apply(auto simp add: errorState_def alpha_equiv_yulcontext_def)
-*)
+        proof(cases "unpack_FunctionDefinition fundef1_def")
+          case Isnt_def1 : None
+
+          have Isnt_def2 : "unpack_FunctionDefinition fundef2_def = None"
+            using Elem_after_fundef F1body F2body Isnt_def1 Fundef1 Fundef2 Fundef1' Fundef2'
+            unfolding Fundefsc_eq_biresc F1body_eq F2body_eq
+            by(auto simp add: alpha_equiv_function_context_elem_def unpack_FunctionDefinition_case)
+
+          then show ?thesis
+            using Hexpr Hrun1 Hrun2 Fc1 Fc2 Args1 Args2 Builtin1 Builtin2 Isnt_builtin1' Isnt_builtin2'
+            using Fundef1 Fundef1' Fundef2 Fundef2' Equiv_after_get_builtin F1body F2body Isnt_def1
+            by(auto simp add: alpha_equiv_yulcontext_def alpha_equiv_function_contexts_def BuiltinFunctionOfIdentifier_def FunctionDefinitionOfIdentifier_def find_map_of_alt unpack_FunctionDefinition_case errorState_def)
+        next
+          case Is_def1 : (Some fnd1)
+
+          then obtain fname1 fargs1 frets1 fstms1 where Fnd1 :
+            "fundef1_def = FunctionDefinition fname1 fargs1 frets1 fstms1"
+            by(cases fundef1_def; auto)
+
+          obtain fnd2 where Is_def2 : "unpack_FunctionDefinition fundef2_def = Some fnd2"
+            using Elem_after_fundef F1body F2body Is_def1 Fundef1 Fundef2 Fundef1' Fundef2'
+            unfolding Fundefsc_eq_biresc F1body_eq F2body_eq
+            by(cases "unpack_FunctionDefinition fundef2_def"; auto simp add: alpha_equiv_function_context_elem_def unpack_FunctionDefinition_case)
+
+          then obtain fname2 fargs2 frets2 fstms2 where Fnd2 :
+            "fundef2_def = FunctionDefinition fname2 fargs2 frets2 fstms2"
+            by(cases fundef2_def; auto)
+
+          have Eq_arg_vals_length :
+            "length args1 = length args2"
+            using Fc1 Fc2 Hexpr
+            by(auto elim:list_all2_lengthD)
+
+          have Eq_fargs_length :
+            "length fargs1 = length fargs2"
+            using Elem_after_fundef F1body_eq F2body_eq F1body F2body Fnd1 Fnd2
+            using Fundefsc_eq_biresc Fundef1' Fundef2'
+            by(cases "length fargs1 = length fargs2"; auto simp add: alpha_equiv_function_context_elem_def)
+
+          have Eq_frets_length :
+            "length frets1 = length frets2"
+            using Elem_after_fundef F1body_eq F2body_eq F1body F2body Fnd1 Fnd2
+            using Fundefsc_eq_biresc Fundef1' Fundef2' Eq_fargs_length
+            by(cases "length frets1 = length frets2"; auto simp add: alpha_equiv_function_context_elem_def)
+
+          show ?thesis
+          proof(cases "length fargs1 = length args1")
+            case Neq_args_length1 : False
+
+            have Neq_args_length2 : "length fargs2 \<noteq> length args2"
+              using Neq_args_length1 Fnd1 Fnd2 Elem_after_fundef F1body_eq F2body_eq Fundef1' Fundef2'
+              using Eq_arg_vals_length
+              by(auto simp add: alpha_equiv_function_context_elem_def)
+
+            show ?thesis 
+              using Hexpr Hctx Hrun1 Hrun2 Fc1 Fc2 Args1 Args2 Builtin1 Builtin2 Isnt_builtin1' Isnt_builtin2'
+              using Fundef1 Fundef2 Fundef1' Fundef2' F1body_eq F2body_eq Fnd1 Fnd2 F1body F2body
+              using Neq_args_length1 Neq_args_length2 Fundefsc_eq_biresc
+              using Equiv_after_fundef Eq_fargs_length Eq_frets_length
+              by(auto simp add: alpha_equiv_yulcontext_def alpha_equiv_function_contexts_def BuiltinFunctionOfIdentifier_def FunctionDefinitionOfIdentifier_def find_map_of_alt errorState_def)
+          next
+            case Eq_args_length1 : True
+
+            have Eq_args_length2 : "length fargs2 = length args2"
+              using Eq_args_length1 Fnd1 Fnd2 Elem_after_fundef F1body_eq F2body_eq Fundef1' Fundef2'
+              using Eq_arg_vals_length Eq_fargs_length
+              by(auto simp add: alpha_equiv_function_context_elem_def)
+
+            obtain bodymode1 bodysc1 where Run_body1 :
+              "run_state (evalStatement n RegularMode (Block fstms1))
+               (biresc1
+                \<lparr>LocalVariables := zip fargs1 (rev argvals1) @ map (\<lambda>v. (v, 0)) frets1,
+                   Functions :=
+                   filter (\<lambda>(name, z). list_contains fundef1_decls name) (Functions biresc1)\<rparr>) =
+               (bodymode1, bodysc1)"
+              by force
+
+            obtain bodymode2 bodysc2 where Run_body2 :
+              "run_state (evalStatement n RegularMode (Block fstms2))
+               (biresc2
+                \<lparr>LocalVariables := zip fargs2 (rev argvals2) @ map (\<lambda>v. (v, 0)) frets2,
+                   Functions :=
+                   filter (\<lambda>(name, z). list_contains fundef2_decls name) (Functions biresc2)\<rparr>) =
+               (bodymode2, bodysc2)"
+              by force
+
+(* next, show equality on final run_state. (IH1 not needed) *)
+
+
+            show ?thesis
+              using Hexpr Hctx Hrun1 Hrun2 Fc1 Fc2 Args1 Args2 Builtin1 Builtin2 Isnt_builtin1' Isnt_builtin2'
+              using Fundef1 Fundef2 Fundef1' Fundef2' F1body_eq F2body_eq Fnd1 Fnd2 F1body F2body
+              using Eq_args_length1 Eq_args_length2 Fundefsc_eq_biresc
+              using Equiv_after_fundef Eq_fargs_length Eq_frets_length
+              using Run_body1 Run_body2
+              apply(auto simp add: alpha_equiv_function_contexts_def BuiltinFunctionOfIdentifier_def FunctionDefinitionOfIdentifier_def find_map_of_alt errorState_def)
+              sorry
+          qed
+        qed
+
       next
         case Is_builtin1 : (Some bivals1)
         then show ?thesis sorry
       qed
-
-        using Hexpr Hctx Hrun1 Hrun2 Fc1 Fc2 Args1 Args2 Builtin1 Builtin2
-        apply(auto simp add: errorState_def alpha_equiv_yulcontext_def)
-
     next
       case Id1 : (Identifier i1)
 
@@ -703,11 +1024,10 @@ next
     qed
   qed
 
-  show ?case sorry
-(*
+  show ?case
     using Conc1 Conc2 Conc3
     by fast
-*)
+
 qed
 
 
